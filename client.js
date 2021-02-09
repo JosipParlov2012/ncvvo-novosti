8d8b386a94d840ebcf6f2abe0dd3321d2854158b
JosipParlov2012 <josipparlov2012@gmail.com>
2021-02-09 21:25:37 +0100

    Bugfix

--- client.js
+++ client.js
@@ -28,7 +28,7 @@ const SELECTOR_PARTIAL_TIME = " > div > div.col-lg-4.col-lg-boxed > div > div > 
 const SELECTOR_PARTIAL_TITLE = " > div > div.col-lg-6.offset-lg-2 > h2 > a";
 const SELECTOR_PARTIAL_TEXT = " > div > div.col-lg-6.offset-lg-2 > div > p";
 
-const REFRESH_URL =  "https://" + process.env.HEROKU_APP_NAME + ".herokuapp.com/refresh";
+const REFRESH_URL =  "https://" + process.env.HEROKU_APP_NAME + ".herokuapp.com";
 
 keyv.on("error", err => console.error("Keyv connection error:\n", err));
 
@@ -41,13 +41,11 @@ app.listen(PORT, () => {
     console.log("Webserver running on port: " + PORT);
 });
 
-app.get("refresh", ((request, response) => {
+app.get("/", ((request, response) => {
     response.send("OK");
 }));
 
-setInterval(() => {
-    fetch(REFRESH_URL);
-}, REFRESH_INTERVAL);
+setInterval(() => fetch(REFRESH_URL), REFRESH_INTERVAL);
 
 function getPartialElement($, latestPath, partial) {
     return $(SELECTOR_ALL_NEWS + " > div." + latestPath + partial);
